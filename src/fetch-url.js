f7206ffacda4655a9e4378d6e78b63e34c67df94
Parlato <alparlat@3c063012601d.ant.amazon.com>
2022-03-16 18:21:09 -0400

    special handling for localhost

--- src/fetch-url.js
+++ src/fetch-url.js
@@ -14,6 +14,14 @@ const fetchUrl = async function (url) {
     const [, , , userName, styleId] = nextUrl.split('/');
     nextUrl = `https://api.mapbox.com/styles/v1/${userName}/${styleId}?title=true&access_token=${mapboxGlAccessToken}`;
   }
+  if (nextUrl.includes('localhost')) {
+    const [preface, address] = nextUrl.split('localhost');
+    // Fetch doesn't accept localhost unless prefaced with http://
+    // This adds the preface if not present
+    if (!preface) {
+      nextUrl = `http://localhost${address}`;
+    }
+  }
   const response = await fetch(nextUrl);
 
   const data = await response.json();
