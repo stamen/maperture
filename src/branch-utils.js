561240ee4bfe1ee83237acc4bfb04884c737bd1b
Parlato <alparlat@3c063012601d.ant.amazon.com>
2022-03-30 11:28:21 -0400

    more reliable parsing

--- src/branch-utils.js
+++ src/branch-utils.js
@@ -1,44 +1,80 @@
-import { branchPattern } from './config';
+import { branchPattern } from "./config";
 
-const isBranchUrl = url => {
+const isBranchUrl = (url) => {
   const { pattern, styles } = branchPattern;
-  const sections = pattern.split('{branch}');
-  const knownUrl = sections.find(s => !s.includes('{style}'));
-  const unknownUrl = sections.find(s => s.includes('{style}'));
+  const sections = pattern.split("{branch}").filter(Boolean);
+  const knownUrlParts = sections.filter((s) => !s.includes("{style}"));
+  const unknownUrlParts = sections.filter((s) => s.includes("{style}"));
 
   return (
-    url.includes(knownUrl) &&
-    styles.some(s => url.includes(unknownUrl.replace('{style}', s)))
+    knownUrlParts.every((part) => url.includes(part)) &&
+    unknownUrlParts.every((part) =>
+      styles.some((s) => url.includes(part.replace("{style}", s)))
+    )
   );
 };
 
-const parseBranchUrl = url => {
+const parseBranchUrl = (url) => {
   const { pattern } = branchPattern;
   if (!isBranchUrl(url)) return null;
-  let branch = '';
-  let styleId = '';
-  const urlPath = url.split('/');
-  pattern.split('/').forEach((section, i) => {
-    if (section === '{branch}') {
-      branch = urlPath[i];
-      return;
+
+  const splitToken = (str, token) => {
+    const arr = str.split(token);
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 !== 0) {
+        arr.splice(i, 0, token);
+      }
     }
-    if (section === '{style}') {
-      styleId = urlPath[i];
-      return;
-    }
-    if (section !== urlPath[i]) {
-      console.error('Problem parsing url.');
-    }
+    return arr;
+  };
+
+  let patternParts = splitToken(pattern, "{branch}");
+  patternParts = patternParts
+    .map((section) => {
+      let nextSection = section;
+      if (section.includes("{style}")) {
+        nextSection = splitToken(section, "{style}");
+      }
+      return nextSection;
+    })
+    .flat()
+    .filter(Boolean);
+
+  let urlTokenValues = [url];
+  // This will leave us with an array of all parts of url that are token values
+  patternParts.forEach((part) => {
+    urlTokenValues = urlTokenValues
+      .map((section) => section.split(part))
+      .flat()
+      .filter(Boolean);
   });
-  return { styleId, branch };
+
+  const tokenOrder = patternParts.filter(
+    (item) => item.startsWith("{") && item.endsWith("}")
+  );
+
+  if (tokenOrder.length !== urlTokenValues.length) {
+    console.error("Trying to parse a url that does not fit the pattern.");
+  }
+
+  const parsed = tokenOrder.reduce((acc, token, i) => {
+    let key = token.replace("{", "").replace("}", "");
+    // Change key name to have better variable
+    if (key === "style") {
+      key = "styleId";
+    }
+    acc[key] = urlTokenValues[i];
+    return acc;
+  }, {});
+
+  return parsed;
 };
 
 const createBranchUrl = (branchName, selectedStyle) => {
   const { pattern } = branchPattern;
   return pattern
-    .replace('{branch}', branchName)
-    .replace('{style}', selectedStyle);
+    .replace("{branch}", branchName)
+    .replace("{style}", selectedStyle);
 };
 
 export { isBranchUrl, parseBranchUrl, createBranchUrl };
