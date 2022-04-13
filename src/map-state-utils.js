e7cd0b5f91ad8c5ec9b79a33f854b611e8aa0ceb
Eric Brelsford <ebrelsford@gmail.com>
2022-04-13 12:50:07 -0400

    Add warning at max pitch

--- src/map-state-utils.js
+++ src/map-state-utils.js
@@ -1,5 +1,21 @@
 import { GOOGLE_MAP_MAX_PITCH } from './constants';
 
+export const checkMapState = (mapState, maps) => {
+  const messages = [];
+
+  const googleMapPresent = maps.some(({ type }) => type === 'google');
+  if (googleMapPresent) {
+    if (mapState.pitch >= GOOGLE_MAP_MAX_PITCH) {
+      messages.push({
+        type: 'warning',
+        message: `Pitch capped at ${GOOGLE_MAP_MAX_PITCH} as that is the maximum for Google Maps.`,
+      });
+    }
+  }
+
+  return messages;
+};
+
 export const validateMapState = (mapState, maps) => {
   const newMapState = { ...mapState };
   const googleMapPresent = maps.some(({ type }) => type === 'google');
