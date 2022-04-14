a06dcb280500f604a6899688af5f5f46c5be4adb
Eric Brelsford <ebrelsford@gmail.com>
2022-04-14 09:58:58 -0400

    Add Mapbox GL max pitch

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const VIEW_MODES = ['swipe', 'mirror', 'phone'];
 export const GOOGLE_MAP_MAX_PITCH_ZOOM_THRESHHOLD = 12;
 export const GOOGLE_MAP_MAX_PITCH_LOW_ZOOM = 35;
 export const GOOGLE_MAP_MAX_PITCH_HIGH_ZOOM = 45;
+export const MAPBOX_GL_MAX_PITCH = 60;
--- src/map-state-utils.js
+++ src/map-state-utils.js
@@ -2,7 +2,10 @@ import {
   GOOGLE_MAP_MAX_PITCH_LOW_ZOOM,
   GOOGLE_MAP_MAX_PITCH_HIGH_ZOOM,
   GOOGLE_MAP_MAX_PITCH_ZOOM_THRESHHOLD,
+  MAPBOX_GL_MAX_PITCH,
 } from './constants';
+
+const googleMapPresent = maps => maps.some(({ type }) => type === 'google');
 
 const getMaxPitch = (mapState, maps) => {
   const googleMapPresent = maps.some(({ type }) => type === 'google');
@@ -11,22 +14,18 @@ const getMaxPitch = (mapState, maps) => {
       ? GOOGLE_MAP_MAX_PITCH_LOW_ZOOM
       : GOOGLE_MAP_MAX_PITCH_HIGH_ZOOM;
   }
-  return Infinity;
+  return MAPBOX_GL_MAX_PITCH;
 };
-
-const googleMapPresent = maps => maps.some(({ type }) => type === 'google');
 
 export const getMapStateMessages = (mapState, maps) => {
   const messages = [];
 
-  if (googleMapPresent(maps)) {
-    const maxPitch = getMaxPitch(mapState, maps);
-    if (mapState.pitch >= maxPitch) {
-      messages.push({
-        type: 'warning',
-        message: `Pitch capped at ${maxPitch} as that is the maximum for Google Maps at this zoom level.`,
-      });
-    }
+  const maxPitch = getMaxPitch(mapState, maps);
+  if (mapState.pitch >= maxPitch) {
+    messages.push({
+      type: 'warning',
+      message: `Pitch capped at ${maxPitch} as that is the maximum for this zoom level.`,
+    });
   }
 
   return messages;
