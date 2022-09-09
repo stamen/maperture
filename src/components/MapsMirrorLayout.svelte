6f4fe71619717222c8651bd155a74cd923138440
Parlato <alparlat@3c063012601d.ant.amazon.com>
2022-09-09 15:09:24 -0400

    inline comments

--- src/components/MapsMirrorLayout.svelte
+++ src/components/MapsMirrorLayout.svelte
@@ -57,6 +57,7 @@
 
   .maps {
     display: flex;
+    /* To account for not showing a border at the edges of the screen */
     height: calc(100% + 2px);
     width: calc(100% + 2px);
     margin-top: -1px;
