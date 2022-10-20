ec736d9ca7694ec73f2ed719762296c255886acb
Alex Parlato <alparlat@3c063012601d.ant.amazon.com>
2022-10-20 10:35:04 -0400

    better error messages

--- src/fetch-url.js
+++ src/fetch-url.js
@@ -17,11 +17,20 @@ const fetchUrl = async url => {
     const [, , , userName, styleId] = nextUrl.split('/');
     nextUrl = `https://api.mapbox.com/styles/v1/${userName}/${styleId}?title=true&access_token=${mapboxGlAccessToken}`;
   }
-  const response = await fetch(nextUrl);
+  let response;
+  try {
+    response = await fetch(nextUrl);
+  } catch (err) {
+    throw { status: err.status, message: err.message };
+  }
+
+  if (!response.ok) {
+    throw { status: response.status, message: response.statusText };
+  }
 
   const data = await response.json();
   // Special handling to error on Mapbox style url since it returns successfully with an error
-  if (urlIsMapbox && data.message) throw new Error(data.message);
+  if (urlIsMapbox && data.message) throw { status: 404, message: data.message };
   return data;
 };
 
