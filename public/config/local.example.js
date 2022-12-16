f1c603fd14d2fc3f0c9d37c1cbd449109d649f84
Eric Brelsford <ebrelsford@gmail.com>
2022-12-16 16:45:17 -0500

    Document gazetteer (#134)
    * Update README
    
    * Update local config example with gazetteer
    
    * Update CHANGELOG

--- public/config/local.example.js
+++ public/config/local.example.js
@@ -1,6 +1,24 @@
 // Replace with your Mapbox GL token, if any
 const mapboxGlAccessToken = 'pk.eyAAAABBBCCC';
 
+// Example of setting up a gazetteer object. This is described in the `README`
+// but these are the options that populate the dropdown in the UI that take you
+// to specific locations or map views. You can add as many as you like here and
+// create groupings (in this example, the only group is called Locations).
+const gazetteer = {
+  Locations: [
+    {
+      'San Francisco, CA': {
+        zoom: 18,
+        center: { lng: -122.4193, lat: 37.7648 },
+      },
+    },
+    {
+      'Washington DC': { zoom: 12, center: { lng: -77.0435, lat: 38.9098 } },
+    },
+  ],
+};
+
 const stylePresets = [
   {
     id: 'mapbox-streets',
@@ -47,4 +65,4 @@ const stylePresets = [
 // Example of stylePresetUrls
 // const stylePresetUrls = ['./presets/example.json'];
 
-export { mapboxGlAccessToken, stylePresets };
+export { gazetteer, mapboxGlAccessToken, stylePresets };
