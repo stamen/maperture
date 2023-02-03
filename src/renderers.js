bd7b31dc743e36ed817fd39e92c4714df0b8086b
Eric Brelsford <ebrelsford@gmail.com>
2023-02-03 10:04:47 -0500

    Add list of renderers

---
+++ src/renderers.js
@@ -0,0 +1,15 @@
+const glVectorRenderers = [
+  { name: 'Mapbox GL JS', value: 'mapbox-gl' },
+  { name: 'Maplibre GL JS', value: 'maplibre-gl' },
+];
+
+const typeToRenderers = {
+  google: [{ name: 'Google Maps', value: 'google' }],
+  leaflet: [{ name: 'Leaflet', value: 'leaflet' }],
+  tangram: [{ name: 'Tangram (Leaflet)', value: 'tangram' }],
+  'vector-gl': glVectorRenderers,
+  'mapbox-gl': glVectorRenderers,
+  'maplibre-gl': glVectorRenderers,
+};
+
+export const getRenderers = map => typeToRenderers[map.type];
