9a798e314adecb7d325990c20c225bb368eab1fd
Alexander Parlato <alexparlato@stamen.com>
2024-10-10 09:53:14 -1000

    add initial check functionality

--- src/components/CustomJsUi.svelte
+++ src/components/CustomJsUi.svelte
@@ -4,6 +4,14 @@
   export let mapId;
   export let mapIdIndex;
 
+  let mapObj;
+  let mounted = false;
+
+  mapObjStore.subscribe(state => {
+    const index = mapIdIndex.split('-').pop();
+    mapObj = state?.[index];
+  });
+
   const { customJs } = $configStore;
 
   $: validActions = customJs.filter(js => js.mapIds.includes(mapId));
@@ -23,66 +31,83 @@
   const onClickDropdown = e => {
     const { dropdown, option } = JSON.parse(e.target.value);
     const opt = dropdownOptions?.[dropdown]?.options?.[option];
-    const index = mapIdIndex.split('-').pop();
-    const map = $mapObjStore?.[index];
-    if (!map || !opt) return;
+    if (!mapObj || !opt) return;
     const { script } = opt;
-    const fn = script(map);
+    const fn = script(mapObj);
     fn();
   };
 
   const onClickCheckbox = script => {
     const index = mapIdIndex.split('-').pop();
-    const map = $mapObjStore?.[index];
-    if (!map || !script) return;
-    const fn = script(map);
+    if (!mapObj || !script) return;
+    const fn = script(mapObj);
     fn();
   };
+
+  const getInitialCheck = checked => {
+    // checked is either a boolean or a factory fn
+    if (typeof checked === 'boolean') {
+      return checked;
+    }
+    if (!mapObj || !checked) return;
+    const fn = checked(mapObj);
+    return fn();
+  };
+
+  $: if (mapObj) {
+    mapObj.on('style.load', () => {
+      mounted = true;
+    });
+  }
 </script>
 
-{#if validActions.length}
-  <div class="action-dropdown">Custom actions:</div>
+{#if mounted}
+  {#if validActions.length}
+    <div class="action-dropdown">Custom actions:</div>
+  {/if}
+
+  <div class="controls">
+    {#if dropdownOptions.length}
+      <select
+        id="custom-actions"
+        bind:value={selectedDropdown}
+        on:change={onClickDropdown}
+      >
+        {#each dropdownOptions as dropdownAction, i}
+          <optgroup label={dropdownAction?.label}>
+            <option value={null} disabled selected hidden
+              >Select action...</option
+            >
+            {#each dropdownAction?.options as dropDownOption, iter}
+              <option value={JSON.stringify({ dropdown: i, option: iter })}>
+                {dropDownOption?.label}
+              </option>
+            {/each}
+          </optgroup>
+        {/each}
+      </select>
+    {/if}
+
+    {#if checkboxOptions.length}
+      <div class="checkbox-container">
+        {#each checkboxOptions as checkboxAction, i}
+          <div class="checkbox-options">
+            <div class="checkbox-label">{checkboxAction?.label}:</div>
+            {#each checkboxAction?.options as checkboxOption, iter}
+              <input
+                type="checkbox"
+                id={iter}
+                checked={getInitialCheck(checkboxOption?.checked)}
+                on:click={() => onClickCheckbox(checkboxOption?.script)}
+              />
+              <label for={iter}>{checkboxOption?.label}</label>
+            {/each}
+          </div>
+        {/each}
+      </div>
+    {/if}
+  </div>
 {/if}
-
-<div class="controls">
-  {#if dropdownOptions.length}
-    <select
-      id="custom-actions"
-      bind:value={selectedDropdown}
-      on:change={onClickDropdown}
-    >
-      {#each dropdownOptions as dropdownAction, i}
-        <optgroup label={dropdownAction?.label}>
-          <option value={null} disabled selected hidden>Select action...</option
-          >
-          {#each dropdownAction?.options as dropDownOption, iter}
-            <option value={JSON.stringify({ dropdown: i, option: iter })}>
-              {dropDownOption?.label}
-            </option>
-          {/each}
-        </optgroup>
-      {/each}
-    </select>
-  {/if}
-
-  {#if checkboxOptions.length}
-    <div class="checkbox-container">
-      {#each checkboxOptions as checkboxAction, i}
-        <div class="checkbox-options">
-          <div class="checkbox-label">{checkboxAction?.label}:</div>
-          {#each checkboxAction?.options as checkboxOption, iter}
-            <input
-              type="checkbox"
-              id={iter}
-              on:click={() => onClickCheckbox(checkboxOption?.script)}
-            />
-            <label for={iter}>{checkboxOption?.label}</label>
-          {/each}
-        </div>
-      {/each}
-    </div>
-  {/if}
-</div>
 
 <style>
   .action-dropdown {
