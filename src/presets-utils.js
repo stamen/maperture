afd4c2b60b4b228836121511c869508836d75fbf
Alexander Parlato <alexparlato@stamen.com>
2025-02-26 13:18:47 -1000

    fix validation

--- src/presets-utils.js
+++ src/presets-utils.js
@@ -2,6 +2,7 @@ export const loadPresetsFromUrl = async url => {
   const response = await fetch(url);
   const json = await response.json();
 
+  console.log(isValidPresetsArray(json));
   if (!isValidPresetsArray(json)) {
     console.warn(`Style presets at ${url} is not valid, check the format`);
     return [];
@@ -14,11 +15,21 @@ const isValidPresetsArray = json => {
   if (!Array.isArray(json)) return false;
 
   const requiredKeys = ['id', 'name', 'type'];
-  const presetsMissingKeys = json.filter(preset => {
+
+  const isValid = preset => {
     const presetKeys = Object.keys(preset);
     const missingKeys = requiredKeys.filter(key => !presetKeys.includes(key));
     return missingKeys.length > 0;
+  };
+
+  const presetsMissingKeys = json.filter(preset => {
+    if (preset.type === 'sublist') {
+      return preset.presets.every(isValid);
+    } else {
+      return isValid(preset);
+    }
   });
+
   if (presetsMissingKeys.length > 0) return false;
   return true;
 };
